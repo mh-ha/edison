82c0dd22f0493e5bcef41533680fd2526a19b903
maengmaengeeee <maengmaengeeee@gmail.com>
2024-03-16 20:47:19 +0900

    240316

---
+++ model/layers.py
@@ -0,0 +1,18 @@
+
+
+
+def feed_forward():
+    pass
+
+def absolute_positional_encoding():
+    pass
+
+def attention():
+    pass
+
+def multi_head_attention():
+    pass
+
+def transformer():
+    pass
+
