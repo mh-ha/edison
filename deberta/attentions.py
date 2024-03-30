7d494f9af0ec4075bcc91ec9b1b4feb3f31bac85
maengmaengeeee <maengmaengeeee@gmail.com>
2024-03-30 15:06:59 +0900

    240330

---
+++ deberta/attentions.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+# class: attention
+
+
+# class: DisentangledSelfAttention
+# option: pre-, post-layernorm
+
+# class: PerceiverAttention
+# option: pre-, post-layernorm
+
+
+
--- deberta/networks.py
+++ deberta/networks.py
@@ -3,11 +3,3 @@
 
 
 
-
-
-
-
-
-
-# class: attention
-
