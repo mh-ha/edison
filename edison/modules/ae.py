b6db01e70fadacf5cd37ad366d808833b7910256
maengmaengeeee <maengmaengeeee@gmail.com>
2024-05-08 21:34:53 +0900

    240508

--- edison/modules/ae.py
+++ edison/modules/ae.py
@@ -1,24 +1,49 @@
+import math
 import torch
-from torch import nn
+from torch import nn, einsum
+import torch.nn.functional as F
 import lightning as L
+
+from einops import rearrange, repeat
 
 from ..config.config import Config
 from ..layers.optimizer import AdamW
+from .positional_embedding import AbsolutePositionalEmbedding
 
+
+#TODO: 작동 확인
 class Autoencoder(L.LightningModule):
     def __init__(self, config:Config):
         super().__init__()
         self.save_hyperparameters()
         self.config = config
-        self.compress_model = None
-        self.reconstruct_model = None
+        #TODO: LM 정의 (BART로)
+        self.lm = None
+        self.lm.freeze()
+        self.lm_encoder = self.lm.get_encoder()
+        self.lm_decoder = self.lm.get_decoder()
+        #TODO: AE 정의(config parameter 정의 필요)
+        self.ae = PerceiverAutoEncoder(
+            dim_lm=config.d_model,
+            num_encoder_latents=config.num_encoder_latents,
+            num_decoder_latents=config.num_decoder_latents,
+            dim_ae=config.dim_ae,
+            depth=config.num_layers,
+            transformer_decoder=True,
+            l2_normalize_latents=config.l2_normalize_latents)
         self.loss = None
         
+    def forward_lm_encoder(self, inputs):
+        return self.lm_encoder(inputs['input_ids'], attention_mask=inputs['attention_mask'])
+    
+    def forward_lm_decoder(self, inputs):
+        return self.lm_decoder(inputs)
+    
     def forward_compressor(self, inputs):
-        return self.compress_model(inputs)
+        return self.ae.perceiver_encoder(inputs)
     
     def forward_reconstructor(self, inputs):
-        return self.reconstruct_model(inputs)
+        return self.ae.perceiver_decoder(inputs)
     
     def forward(self, inputs):
         comp_output = self.forward_compressor(inputs)
@@ -27,7 +52,9 @@ class Autoencoder(L.LightningModule):
 
     def training_step(self, batch, batch_idx):
         inputs, target = batch
-        output = self.forward(inputs)
+        lm_encoder_outputs = self.forward_lm_encoder(inputs)
+        output = self.forward(lm_encoder_outputs['last_hidden_state'])
+        output = self.forward_lm_decoder(output)
         loss = self.loss(output, target)
         return loss
 
@@ -39,3 +66,307 @@ class Autoencoder(L.LightningModule):
                 'interval': 'epoch',
                 'frequency': 1,}
             }
+        
+
+
+
+def exists(x):
+    return x is not None
+
+def divisible_by(numer, denom):
+    return (numer % denom) == 0
+
+
+# NN components
+class LayerNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.ones(dim))
+        self.register_buffer("beta", torch.zeros(dim))
+
+    def forward(self, x):
+        return F.layer_norm(x, x.shape[-1:], self.gamma, self.beta)
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim, eps=1e-8):
+        super().__init__()
+        self.scale = dim ** -0.5
+        self.eps = eps
+        self.gamma = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        norm = torch.norm(x, dim=-1, keepdim=True) * self.scale
+        return x / norm.clamp(min=self.eps) * self.gamma
+
+
+def FeedForward(dim, mult=4, dropout=0.):
+    hidden_dim = int(dim * mult)
+    return nn.Sequential(
+        nn.LayerNorm(dim),
+        nn.Linear(dim, hidden_dim),
+        nn.GELU(),
+        nn.Dropout(dropout),
+        nn.Linear(hidden_dim, dim)
+    )
+
+# Standard attention
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        qk_norm=True,
+    ):
+        super().__init__()
+        hidden_dim = dim
+        heads = dim // dim_head
+        assert divisible_by(dim, heads), 'dimension must be divisible by number of heads'
+
+
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+
+        self.norm = nn.LayerNorm(dim) 
+
+        self.query_norm = RMSNorm(dim_head) if qk_norm else nn.Identity()
+        self.key_norm = RMSNorm(dim_head) if qk_norm else nn.Identity()
+
+        self.to_q = nn.Linear(dim, hidden_dim, bias = False)
+        self.to_k = nn.Linear(dim, hidden_dim, bias = False)
+        self.to_v = nn.Linear(dim, hidden_dim, bias = False)
+        self.to_out = nn.Linear(hidden_dim, dim)
+
+    def forward(
+        self,
+        x,
+    ):
+        h = self.heads
+
+        x = self.norm(x)
+
+
+        qkv = (self.to_q(x), self.to_k(x), self.to_v(x))
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        sim = einsum('b h i d, b h j d -> b h i j', self.query_norm(q)* self.scale, self.key_norm(k))
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+# attention pooling
+
+class PerceiverAttention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        dim_latent,
+        dim_head=64,
+        qk_norm=True,
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+
+        inner_dim = max(dim_latent, dim)
+        self.heads = inner_dim // dim_head
+
+        self.norm = nn.LayerNorm(dim)
+        self.norm_latents = nn.LayerNorm(dim_latent)
+
+        self.query_norm = RMSNorm(dim_head) if qk_norm else nn.Identity()
+        self.key_norm = RMSNorm(dim_head) if qk_norm else nn.Identity()
+
+        self.to_q = nn.Linear(dim_latent, inner_dim, bias=False)
+        if dim_latent != dim:
+            self.latent_to_kv = nn.Linear(dim_latent, inner_dim * 2, bias=False)
+        else:
+            self.latent_to_kv = None
+        self.to_kv = nn.Linear(dim, inner_dim * 2, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim_latent),
+        )
+
+    def forward(self, x, latents, mask=None):
+        # x, latent normalization
+        x = self.norm(x)  # 1: (batch, max_seq_len, dim) -> (batch, max_seq_len, dim)
+        latents = self.norm_latents(latents)  # 2 : (batch, num_latents, dim_latent) -> (batch, num_latents, dim_latent)
+
+        b, h = x.shape[0], self.heads
+
+        # latents: latent_dim -> inner_dim
+        q = self.to_q(latents)  # 3: (batch, num_latents, dim_latent) -> (batch, num_latents, inner_dim(=max(dim, dim_latent)))
+        # the paper differs from Perceiver in which they also concat the key / values derived from the latents to be attended to
+        if exists(self.latent_to_kv):
+            # concat kv(max_seq_len), latents(num_latents) along the sequence dimension (dim=1)
+            kv_input = torch.cat([self.to_kv(x), self.latent_to_kv(latents)], dim=1)  # 4, 5: concat kv(max_seq_len), latents(num_latents) along the sequence dimension
+        else:
+            kv_input = torch.cat([self.to_kv(x), self.to_kv(latents)], dim=1)
+        # split kv_input into k, v (batch, max_seq_len + num_latents, inner_dim)
+        k, v = rearrange(kv_input, 'b n (split d) -> split b n d', split=2)  # (batch, max_seq_len + num_latents, inner_dim*2) -> (2, batch, max_seq_len + num_latents, inner_dim)
+
+        # split q, k, v into heads(=inner_dim/dim_head)
+        q, k, v = map(lambda t: rearrange(
+            t, 'b n (h d) -> b h n d', h=h), (q, k, v))  # (batch, (num_latents) or (max_seq_len + num_latents), inner_dim) -> (batch, heads, (num_latents) or (max_seq_len + num_latents), dim_head)
+
+        # similarities and masking
+        # sim=(q@k)/sqrt(dim_head) (batch, heads, num_latents, max_seq_len + num_latents)
+        sim = einsum('... i d, ... j d  -> ... i j',
+                     self.query_norm(q) * self.scale, self.key_norm(k))  # 6, 7: (batch, heads, num_latents, dim_head) @ (batch, heads, max_seq_len + num_latents, dim_head) -> (batch, heads, num_latents, max_seq_len + num_latents)
+
+        if exists(mask):
+            max_neg_value = -torch.finfo(sim.dtype).max
+            mask = F.pad(mask, (0, latents.shape[-2]), value=True)
+            mask = rearrange(mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~mask, max_neg_value)
+
+        # softmax and multiply with values
+        attn = sim.softmax(dim=-1, dtype=torch.float32)
+        attn = attn.to(sim.dtype)
+        # out = attn@v (batch, heads, num_latents, dim_head)
+        out = einsum('... i j, ... j d -> ... i d', attn, v)  # (batch, heads, num_latents, max_seq_len + num_latents) * (batch, heads, max_seq_len + num_latents, dim_head) -> (batch, heads, num_latents, dim_head)
+        # merge heads
+        out = rearrange(out, 'b h n d -> b n (h d)', h=h)  # (batch, heads, num_latents, dim_head) -> (batch, num_latents, inner_dim)
+        # linear projection (batch, num_latents, dim_latent)
+        return self.to_out(out)  # 8: (batch, num_latents, inner_dim) -> (batch, num_latents, dim_latent)
+
+
+class PerceiverResampler(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        dim_latent,
+        depth,
+        dim_head=64,
+        num_latents=16,
+        max_seq_len=64,
+        ff_mult=4,
+        legacy=False,
+        l2_normalize_latents=False,
+    ):
+        super().__init__()
+        self.pos_emb = AbsolutePositionalEmbedding(dim, max_seq_len)
+
+        if legacy:
+            dim_out = dim_latent
+            dim_latent = dim
+
+        self.latents = nn.Parameter(torch.randn(num_latents, dim_latent))
+        nn.init.normal_(self.latents, std = 0.02)
+
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PerceiverAttention(
+                    dim=dim, dim_latent=dim_latent, dim_head=dim_head),
+                FeedForward(dim=dim_latent, mult=ff_mult)
+            ]))
+
+        self.l2_normalize_latents = l2_normalize_latents
+
+        self.final_norm = nn.LayerNorm(dim_latent)
+        self.output_proj = nn.Linear(dim_latent, dim_out) if legacy else nn.Identity()
+
+    def forward(self, x, mask=None):
+        pos_emb = self.pos_emb(x)
+
+        x_with_pos = x + pos_emb
+
+        latents = repeat(self.latents, 'n d -> b n d', b=x.shape[0])
+
+        for attn, ff in self.layers:
+            latents = attn(x_with_pos, latents, mask=mask) + latents
+            latents = ff(latents) + latents
+        latents = self.output_proj(self.final_norm(latents))
+        # Normalize latents to norm sqrt(d_latent)
+        if self.l2_normalize_latents:
+            latents = F.normalize(latents, dim=-1) * math.sqrt(latents.shape[-1])
+        return latents
+        
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim_input,
+        dim_tx,
+        depth,
+        dim_head=64,
+        max_seq_len=64,
+        ff_mult=4,
+    ):
+        super().__init__()
+        self.pos_emb = AbsolutePositionalEmbedding(dim_tx, max_seq_len)
+
+        self.input_proj = nn.Linear(dim_input, dim_tx)
+
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(
+                    dim=dim_tx, dim_head=dim_head),
+                FeedForward(dim=dim_tx, mult=ff_mult)
+            ]))
+
+        self.final_norm = nn.LayerNorm(dim_tx)
+        self.output_proj = nn.Identity()
+
+    def forward(self, x, mask=None):
+        
+        assert not exists(mask)
+        x = self.input_proj(x)
+        pos_emb = self.pos_emb(x)
+        x = x + pos_emb
+
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        return self.output_proj(self.final_norm(x))
+
+
+class PerceiverAutoEncoder(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim_lm,
+        dim_ae,
+        depth,
+        dim_head=64,
+        num_encoder_latents=8,
+        num_decoder_latents=32,
+        max_seq_len=64,
+        ff_mult=4,
+        encoder_only=False,
+        transformer_decoder=False,
+        l2_normalize_latents=False,
+    ):
+        super().__init__()
+        self.encoder_only = encoder_only
+        if self.encoder_only:
+            assert dim_ae == dim_lm
+        self.perceiver_encoder = PerceiverResampler(dim=dim_lm, dim_latent=dim_ae, depth=depth, dim_head=dim_head,
+                                                    num_latents=num_encoder_latents, max_seq_len=max_seq_len, ff_mult=ff_mult, l2_normalize_latents=l2_normalize_latents)
+        if transformer_decoder:
+            self.perceiver_decoder = Transformer(dim_input=dim_ae, dim_tx=dim_lm, depth=depth, dim_head=dim_head, max_seq_len=num_encoder_latents, ff_mult=ff_mult)
+        else:
+            self.perceiver_decoder = PerceiverResampler(dim=dim_ae, dim_latent=dim_lm, depth=depth, dim_head=dim_head,
+                                                        num_latents=num_decoder_latents, max_seq_len=num_encoder_latents, ff_mult=ff_mult)
+
+    def decode(self, ae_latent):
+        return self.perceiver_decoder(ae_latent)
+    
+    def encode(self, encoder_outputs, attention_mask):
+        return self.perceiver_encoder(encoder_outputs, mask=attention_mask.bool())
+
+    def forward(self, encoder_outputs, attention_mask):
+        encoder_latents = self.perceiver_encoder(
+            encoder_outputs, mask=attention_mask.bool())
+        return self.perceiver_decoder(encoder_latents)
+        
+
