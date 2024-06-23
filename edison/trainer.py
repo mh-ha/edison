5ff0940639bcfc68fe4121ca411433cc01a71558
maeng.k <maeng.k@hpcnt.com>
2024-06-23 10:52:46 +0900

    240623

--- edison/trainer.py
+++ edison/trainer.py
@@ -8,7 +8,7 @@ from .config.config import Config
 def get_trainer(config: Config):
     trainer = L.Trainer(
         # max_epochs=10,
-        strategy=DDPStrategy(find_unused_parameters=True),
+        # strategy=DDPStrategy(find_unused_parameters=True),
         max_steps=500000,
         precision=16,
         gradient_clip_val=config.gradient_clip_val,
