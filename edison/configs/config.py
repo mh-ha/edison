99f1245f78a67a737362c415dd98bf4b364ae607
maeng.k <maeng.k@hpcnt.com>
2024-07-15 01:47:31 +0900

    240714

--- edison/configs/config.py
+++ edison/configs/config.py
@@ -189,7 +189,7 @@ class EdisonConfig(Config):
     encoding_mode: str = 'sentence_only'  # 'sentence_only', 'both_separately', 'both_together'
     #############################
     # Edison Diffusion
-    pretrained_ae_path: str = 'lightning_logs/edison_ae/epoch=1377-step=500000.ckpt'
+    pretrained_ae_path: str = 'lightning_logs/edison_ae/checkpoints/epoch=1377-step=500000.ckpt'
     sampling_timesteps: int = 250
     loss_type: str = 'l2'
     objective: str = 'pred_v'
