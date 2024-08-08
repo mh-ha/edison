bb421ae1f2181ef4492a092507bacd995d3ab9c9
maeng.k <maeng.k@hpcnt.com>
2024-08-08 22:31:07 +0900

    240808

--- test.py
+++ test.py
@@ -1,16 +1,47 @@
 import os
 import argparse
 import warnings
+from typing import Optional
 
 from lightning.pytorch.loggers import WandbLogger
 
 from edison.modules import get_module
 from edison.modules.base import BaseEdisonDiffusion
 from edison.configs.base import Config
-from edison.pipes.evaluate import evaluate_trained_model
+from edison.pipes.generate import generate_from_model
+from edison.modules.lightning_data_modules import get_dataset
+from edison.metrics.evaluation import evaluate_model
 
 os.environ['CURL_CA_BUNDLE'] = ''
 warnings.filterwarnings("ignore")
+
+
+def evaluate_trained_model(
+    model: BaseEdisonDiffusion,
+    saved_file_name='generated_samples.csv',
+    wandb_logger: Optional[WandbLogger] = None
+):
+    model.eval()
+    # model.cuda()
+    dataset = get_dataset('roc')
+    reference_data = dataset['valid']['text'] + dataset['test']['text']
+    generated_data = generate_from_model(
+        model=model,
+        num_samples=5000,
+        batch_size=128,
+        seq_len=64,
+        saved_file_name=saved_file_name
+    )
+    generated_data = generated_data['text'].tolist()
+
+    for i in range(5):
+        gen = generated_data[i*1000:(i+1)*1000]
+        ref = reference_data[i*1000:(i+1)*1000]
+        result = evaluate_model(gen, ref)
+        if wandb_logger:
+            wandb_logger.log_metrics(result, step=i)
+            wandb_logger.log_table(key=f"text_generated_{i}", columns=['text'], data=[[text] for text in gen])
+            wandb_logger.log_table(key=f"text_reference_{i}", columns=['text'], data=[[text] for text in ref])
 
 
 def main():
@@ -39,7 +70,6 @@ def main():
         autoencoder=None
     )
     print(f"[evaluation.main] Model loaded from {args.checkpoint_path}.")
-    evaluate_trained_model(model, wandb_logger=wandb_logger)
 
 
 if __name__ == '__main__':
