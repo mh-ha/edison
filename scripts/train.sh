9d3d30709d6cc0d8eb7c7c7dd09c719f473ac2e3
maengmaengeeee <maengmaengeeee@gmail.com>
2024-08-30 15:49:52 +0900

    240830

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
-python run.py --run_name "baseline_$current_time"
+python run.py --run_name "latent_diffusion_1gpu_$current_time" --batch_size 256
+
+current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+python run.py --run_name "latent_diffusion_8gpu_$current_time" --batch_size 32
+
+current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+python run_discrete_diffusion.py --run_name "word_diffusion_1gpu_$current_time" --batch_size 256
+
+current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+python run_discrete_diffusion.py --run_name "word_diffusion_8gpu_$current_time" --batch_size 32
