4fd44fac0132a68c8d21519b9cc48f7a9a9d4c68
maeng.k <maeng.k@hpcnt.com>
2024-09-01 18:20:14 +0900

    240901

--- run_discrete_diffusion.py
+++ run_discrete_diffusion.py
@@ -25,12 +25,12 @@ def main():
         )
     else:
         config = DiscreteDiffusionConfig()
-    # wandb_logger = WandbLogger(
-    #     project=config.project_name,
-    #     config=config.__dict__,
-    #     name=args.run_name,
-    #     log_model=True,)
-    wandb_logger = None
+    wandb_logger = WandbLogger(
+        project=config.project_name,
+        config=config.__dict__,
+        name=args.run_name,
+        log_model=True,)
+    # wandb_logger = None
     print(config)
     model = train(config, wandb_logger=wandb_logger)
     # evaluate_trained_model(model, wandb_logger=wandb_logger)
