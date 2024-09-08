588f3e8ecc5cde1373031d41a56249ee9786d31c
maeng.k <maeng.k@hpcnt.com>
2024-09-09 00:19:32 +0900

    240909

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,11 +6,11 @@ spacy download en_core_web_sm
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 python run.py --run_name "latent_diffusion_1gpu_$current_time" --batch_size 256
 
-current_time=$(date +"%Y-%m-%d_%H-%M-%S")
-python run.py --run_name "latent_diffusion_8gpu_$current_time" --batch_size 32
+# current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+# python run.py --run_name "latent_diffusion_8gpu_$current_time" --batch_size 32
 
-current_time=$(date +"%Y-%m-%d_%H-%M-%S")
-python run_discrete_diffusion.py --run_name "word_diffusion_1gpu_$current_time" --batch_size 256
+# current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+# python run_discrete_diffusion.py --run_name "word_diffusion_1gpu_$current_time" --batch_size 256
 
-current_time=$(date +"%Y-%m-%d_%H-%M-%S")
-python run_discrete_diffusion.py --run_name "word_diffusion_8gpu_$current_time" --batch_size 32
+# current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+# python run_discrete_diffusion.py --run_name "word_diffusion_8gpu_$current_time" --batch_size 32
